90fb1ed606245c8617ed36ea85f88a0824c8a8db
kim-kiwon <kimkiwonn@gmail.com>
2020-12-30 21:42:55 +0900

    12.30 Silver2

---
+++ DP/[11722]가장 긴 감소하는 부분수열.py
@@ -0,0 +1,9 @@
+N = int(input())
+arr = list(map(int, input().split()))
+DP = [1 for _ in range(N)]
+
+for i in range(1,N):
+    for j in range(i):
+        if arr[j] > arr[i]:
+            DP[i] = max(DP[i], DP[j]+1)
+print(max(DP))
