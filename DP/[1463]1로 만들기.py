9d30841ad8dee927acc8cfbdae16fe8319b46af3
kim-kiwon <kimkiwonn@gmail.com>
2021-01-03 23:10:58 +0900

    1.3 Silver3

---
+++ DP/[1463]1로 만들기.py
@@ -0,0 +1,11 @@
+N = int(input())
+DP = [0 for _ in range(N+1)]
+
+for i in range(2, N+1):
+    DP[i] = DP[i-1] + 1
+    if i % 2 == 0 and DP[i] > DP[i//2] + 1:
+        DP[i] = DP[i//2] + 1
+    if i % 3 == 0 and DP[i] > DP[i//3] + 1:
+        DP[i] = DP[i//3] + 1
+
+print(DP[N])
